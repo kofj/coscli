2ae09acdceea1f5f3a2ae97ea3160f280490680e
willppan <willppan@tencent.com>
2025-07-31 17:15:47 +0800

    创建存储桶修改

--- cmd/mb.go
+++ cmd/mb.go
@@ -42,12 +42,26 @@ func init() {
 	mbCmd.Flags().StringP("region", "r", "", "Region")
 	mbCmd.Flags().BoolP("ofs", "o", false, "Ofs")
 	mbCmd.Flags().BoolP("maz", "m", false, "Maz")
+	mbCmd.Flags().String("acl", "", "Defines the Access Control List (ACL) property of an bucket. The default value is default.")
+	mbCmd.Flags().String("grant-read", "", "Grants the grantee permission to read the bucket. The format is id=\"[OwnerUin]\", for example, id=\"100000000001\". Multiple grantees can be specified using commas (,), for example, id=\"100000000001\",id=\"100000000002\".")
+	mbCmd.Flags().String("grant-write", "", "Grants the grantee permission to write the object. The format is id=\"[OwnerUin]\", for example, id=\"100000000001\". Multiple grantees can be specified using commas (,), for example, id='100000000001',id=\"100000000002\".")
+	mbCmd.Flags().String("grant-read-acp", "", "Grants the grantee permission to read the bucket's Access Control List (ACL). The format is id=\"[OwnerUin]\", for example, id=\"100000000001\". Multiple grantees can be specified using commas (,), for example, id=\"100000000001\",id=\"100000000002\".")
+	mbCmd.Flags().String("grant-write-acp", "", "Grants the grantee permission to write the bucket's Access Control List (ACL). The format is id=\"[OwnerUin]\", for example, id=\"100000000001\". Multiple grantees can be specified using commas (,), for example, id=\"100000000001\",id=\"100000000002\".")
+	mbCmd.Flags().String("grant-full-control", "", "Grants the grantee full permissions to operate on the bucket. The format is id=\"[OwnerUin]\", for example, id=\"100000000001\". Multiple grantees can be specified using commas (,), for example, id=\"100000000001\",id=\"100000000002\".")
+	mbCmd.Flags().String("tags", "", "The set of tags for the bucket, with a maximum of 10 tags (e.g., Key1=Value1 & Key2=Value2). The Key and Value in the tag set must be URL-encoded beforehand.")
 }
 
 func createBucket(cmd *cobra.Command, args []string) error {
 	flagRegion, _ := cmd.Flags().GetString("region")
 	flagOfs, _ := cmd.Flags().GetBool("ofs")
 	flagMaz, _ := cmd.Flags().GetBool("maz")
+	acl, _ := cmd.Flags().GetString("acl")
+	grantRead, _ := cmd.Flags().GetString("grant-read")
+	grantWrite, _ := cmd.Flags().GetString("grant-write")
+	grantReadACP, _ := cmd.Flags().GetString("grant-read-acp")
+	grantWriteACP, _ := cmd.Flags().GetString("grant-write-acp")
+	grantFullControl, _ := cmd.Flags().GetString("grant-full-control")
+	tags, _ := cmd.Flags().GetString("tags")
 	if param.Endpoint == "" && flagRegion != "" {
 		param.Endpoint = fmt.Sprintf("cos.%s.myqcloud.com", flagRegion)
 	}
@@ -57,14 +71,22 @@ func createBucket(cmd *cobra.Command, args []string) error {
 	if err != nil {
 		return err
 	}
+
+	// 解析tags
+	tags, err = util.EncodeTagging(tags)
+	if err != nil {
+		return err
+	}
+
 	opt := &cos.BucketPutOptions{
-		XCosACL:                   "",
-		XCosGrantRead:             "",
-		XCosGrantWrite:            "",
-		XCosGrantFullControl:      "",
-		XCosGrantReadACP:          "",
-		XCosGrantWriteACP:         "",
+		XCosACL:                   acl,
+		XCosGrantRead:             grantRead,
+		XCosGrantWrite:            grantWrite,
+		XCosGrantFullControl:      grantFullControl,
+		XCosGrantReadACP:          grantReadACP,
+		XCosGrantWriteACP:         grantWriteACP,
 		CreateBucketConfiguration: &cos.CreateBucketConfiguration{},
+		XCosTagging:               tags,
 	}
 
 	if flagOfs {
