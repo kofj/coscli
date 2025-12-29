b13a85ca5523c13b2dbad9abf329bd0d65023402
willppan <willppan@tencent.com>
2025-12-29 11:45:13 +0800

    signurl增加参数

--- cmd/signurl.go
+++ cmd/signurl.go
@@ -26,10 +26,10 @@ Example:
 	Args: cobra.ExactArgs(1),
 	RunE: func(cmd *cobra.Command, args []string) error {
 		time, _ := cmd.Flags().GetInt("time")
-		mode, _ := cmd.Flags().GetString("mode")
+		simpleOutput, _ := cmd.Flags().GetBool("simple-output")
 		var err error
 		if util.IsCosPath(args[0]) {
-			err = GetSignedURL(args[0], time, mode)
+			err = GetSignedURL(args[0], time, simpleOutput)
 		} else {
 			return fmt.Errorf("cospath needs to contain cos://")
 		}
@@ -42,11 +42,11 @@ func init() {
 	rootCmd.AddCommand(signurlCmd)
 
 	signurlCmd.Flags().IntP("time", "t", 10000, "Set the validity time of the signature(Default 10000)")
-	signurlCmd.Flags().StringP("mode", "m", "", "Set output mode")
+	signurlCmd.Flags().BoolP("simple-output", "", false, "Set simple output mode")
 }
 
 // GetSignedURL 生成签名url
-func GetSignedURL(path string, t int, mode string) error {
+func GetSignedURL(path string, t int, simpleOutput bool) error {
 	bucketName, cosPath := util.ParsePath(path)
 	c, err := util.NewClient(&config, &param, bucketName)
 	if err != nil {
@@ -63,7 +63,7 @@ func GetSignedURL(path string, t int, mode string) error {
 		return err
 	}
 
-	if mode == "simple" {
+	if simpleOutput {
 		fmt.Println(presignedURL)
 	} else {
 		logger.Infoln("Signed URL:")
